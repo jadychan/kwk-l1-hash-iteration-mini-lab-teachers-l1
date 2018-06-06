ce0d21a08c37cae1b3d4eadf7a5f038fb8ca7b62
jadychan <jadychan123@gmail.com>
2018-06-06 06:31:12 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -41,4 +41,11 @@ end
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+  olympics_hash = {
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
+    :London => "2012"
+  }
+  olympics_hash[:Atlanta] = "1996"
 end
