16b7039f8de82a819bd405a71c3b8c453833f374
jadychan <jadychan123@gmail.com>
2018-06-06 06:27:25 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -35,7 +35,7 @@ def iterate_through_hash
 
   olympics_places = []
   olympics_hash.each do |place|
-    olympics_places.push(place.key.upcase)
+    olympics_places.push(olympics_hash.key
   end
 
   puts olympics_places
