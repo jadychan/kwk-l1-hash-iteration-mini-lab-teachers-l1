18d2134fdd4ad6564c53224243aecc470760bb74
jadychan <jadychan123@gmail.com>
2018-06-06 06:29:23 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -34,6 +34,7 @@ def iterate_through_hash
   olympics_hash[:Atlanta] = "1996"
 
   olympics_places = []
+  olympics_counter = 0
   while olympics_counter < olympics_hash.size
     olympics_places.push(olympics_hash.key().upcase)
   end
