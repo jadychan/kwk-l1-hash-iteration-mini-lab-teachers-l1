cb64f552acd81f75c41b9744d3d6c42683d5b0c9
jadychan <jadychan123@gmail.com>
2018-06-06 06:16:32 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -32,6 +32,7 @@ def iterate_through_hash
     :London => "2012"
   }
   olympics_hash[:Atlanta] = "1996"
+  
   olympics_hash.each do |place, year|
     puts "The #{place.upcase} summer olympics took place in #{year}."
   end
